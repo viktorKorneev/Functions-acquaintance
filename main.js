8d4f76a7b93c4834d8750adfc8f395640124ec64
viktorKorneev <korneevviktor90@yandex.ru>
2024-07-29 16:01:54 +0300

    step-3

--- main.js
+++ main.js
@@ -27,10 +27,12 @@ if (dogWeight > 20) {
     console.log(dogName + " says woof woof");
 }
 
-function bark (name, weight) {
-    if (weight > 20) {
-        console.log(name + " says WOOF WOOF");
+function bark (name, weight) { //Имя фукции и (параметры функции)
+    if (weight > 20) {                           // Блок кода тело функции, в теле функции описываем что будет- 
+        console.log(name + " says WOOF WOOF");   // -просходить с параметрами.
     } else {
         console.log(name + " says woof woof");
     }
 }
+bark("rover", 23) // Вызываем функцию по имени. Оператор вызовая функции () 
+                    // Передаём в качестве параметров имя собаки и вес
