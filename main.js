9948aabad8f4f745191bfe57524202f58b53925c
viktorKorneev <korneevviktor90@yandex.ru>
2024-07-29 17:49:16 +0300

    step-10

--- main.js
+++ main.js
@@ -12,24 +12,41 @@
 // bark("spike", 53); // Передаём аргументы в правильном порядке, параметры === аргументы
 // bark("lady", 17);
 
-function makeTea(cups, tea) {
-    console.log("Brewing " + cups + " cups of " + tea);
+// function makeTea(cups, tea) {
+//     console.log("Brewing " + cups + " cups of " + tea);
+// }
+
+// // правильный вызов функции
+// makeTea(3, "Early Grey")
+
+// // недостающие аргументы функции
+// makeTea(3)       // undefined
+
+// // "лишние" аргументы функции
+// makeTea(3, "Early Grey", "hey ma!", 42);
+
+// // агрументы переданы в неверном порядке
+// makeTea("Early Grey", 3)
+
+// // функция без параметров
+// function barkAtTheMoon() {
+//     console.log("Woooooooooooooooooooo!");
+// }
+// barkAtTheMoon();
+
+// deposit
+const depositAmount = 1000
+const yearlyRate = 15
+const depositTermInYears = 2
+
+let depositIncome = 0
+let count = 0
+
+while (count < depositTermInYears) {
+    depositIncome = depositIncome + depositAmount * (yearlyRate / 100)
+    count  = count + 1
 }
 
-// правильный вызов функции
-makeTea(3, "Early Grey")
+// total
 
-// недостающие аргументы функции
-makeTea(3)       // undefined
-
-// "лишние" аргументы функции
-makeTea(3, "Early Grey", "hey ma!", 42);
-
-// агрументы переданы в неверном порядке
-makeTea("Early Grey", 3)
-
-// функция без параметров
-function barkAtTheMoon() {
-    console.log("Woooooooooooooooooooo!");
-}
-barkAtTheMoon();
+console.log(depositIncome);
